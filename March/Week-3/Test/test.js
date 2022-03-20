13c95d44128361958677f21cd3c0035f34f7eeb3
sathishl26 <sathish.skraja@gmail.com>
2022-03-20 19:13:26 +0530

    test commit

---
+++ March/Week-3/Test/test.js
@@ -0,0 +1,18 @@
+function loop(n){
+    for(i=1; i<=n; i++){
+        if (i%3 == 0 && i%5 == 0){
+            console.log(i,"fizzbuzz")
+        }
+        else if (i%3 == 0){
+            console.log(i, "fizz");
+        }
+        else if (i%5 == 0){
+            console.log(i, "buzz")
+        }
+        else {
+            console.log(i)
+        }
+    }
+}
+
+loop(100);
